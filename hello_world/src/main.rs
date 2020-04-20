45cc1bb52e339e566ad4778359c89b195195cea9
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-20 16:04:46 +0200

    variables and argument in strings

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
     println!("Hello, world!");
     println!("Using arguments: 1. {0}  and 2. {1}","ARGUMENT","another argument");
+    println!("Using arguments: 1. {argument}  and 2. {another_argument}", argument="ARGUMENT", another_argument="another argument");
 }
