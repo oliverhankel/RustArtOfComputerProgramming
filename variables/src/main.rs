bb81495e9bbeb6783b6eec1c8dd71537a04470a4
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-21 08:44:11 +0200

    Variables & Conditions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
-    let x =5;
-    println!("{}",x);
+    let y = 1;
+    let mut x =5; // let mut ==> mut means that it is mutable
+    x = 7;
+    println!("x = {0}; y = {1}",x,y);
 }
