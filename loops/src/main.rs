a25cd19929f68cd093d5594d58fe92906391dc05
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-21 13:29:52 +0200

    Loop: for updated

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -6,6 +6,20 @@ fn main() {
         x = x-1; 
     }
     
+   
 
+    for (i,j) in (0..5).enumerate() 
+    {
+        println!("i is now in the for loop: {}",i);
+        println!("j is now in the for loop: {}",j);
+    }
+    
+
+    for i in 0..5
+    {
+        println!("i is now in the for loop: {}",i);
+
+    }
+    
 
 }
