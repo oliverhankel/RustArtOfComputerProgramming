ecec8812e2630c9ef9f6bccc79a302b2f64dcd4b
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-21 10:55:38 +0200

    Conditions updated

--- conditions/src/main.rs
+++ conditions/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+   let x = 5;
+
+   if x == 5 {
+     println!("X is very likely 5");
+   } else {   // could also be a else if
+     println!("X is not very likely 5");
+   }
+
+
+    
 }
