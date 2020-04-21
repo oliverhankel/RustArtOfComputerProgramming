331a9263cdb133e0ca282e52d7867fdccfe6054f
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-21 19:40:44 +0200

    updated with named loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -6,8 +6,6 @@ fn main() {
         x = x-1; 
     }
     
-   
-
     for (i,j) in (0..5).enumerate() 
     {
         println!("i is now in the for loop: {}",i);
@@ -18,7 +16,26 @@ fn main() {
     for i in 0..5
     {
         println!("i is now in the for loop: {}",i);
+        if i==3 {
+            println!("BREAK");
+            break;
+        }
 
+    }
+
+    'name_outer_loop: for a in 0..5 {
+        println!("in outer loop: {}",a);
+
+        'name_inner_loop: for b in 0..10 {
+
+            if b == 7 {
+                break 'name_inner_loop;
+            }
+
+            println!("in inner loop: {}", b)
+
+
+        }
     }
     
 
