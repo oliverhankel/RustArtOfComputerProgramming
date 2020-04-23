c50f904909fd625ceede6792b00a498bf23c511f
Oliver Hankel <oliver.hankel@gmail.com>
2020-04-23 07:53:17 +0200

    Datatypes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,30 @@
 fn main() {
     let y = 1;
     let mut x =5; // let mut ==> mut means that it is mutable
+    println!("x is: {}", x);
     x = 7;
+    
+    //Visibility of variables
+
+    { // Scope of x within the brackets
+        let x = 9;
+        println!("x within bracket: {}", x);
+    
+    }
+
     println!("x = {0}; y = {1}",x,y);
+
+    // data types:
+
+    let a: bool = true;
+
+    let chara: char = 'ü is a umlaut'; 
+
+    if a 
+    {
+        println!("{}",chara);
+    }
+
+
+
 }
