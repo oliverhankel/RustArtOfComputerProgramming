80835ca4599cab25c61d565518f3b1098d726d83
Oliver Hankel <oliver.hankel@gmail.com>
2020-05-11 15:05:06 +0200

    functions in variables

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+  
+    print_me(42);
+    let mut retur:i32;
+
+    let boolean_list: [bool;5] = [true,false,false,true,true];
+
+    for y in &boolean_list
+    {
+        println!("{}",y);
+        if *y
+        {
+            retur = return_integer(1,2,3);
+        }
+        else
+        {
+            retur = return_integer_2(1,2,3);
+        }
+        println!("{}",retur);
+    }
+    
 }
+
+fn print_me(x: i32) {
+
+    println!("in print_me: {}",x);
+
+}
+
+fn return_integer(a: i32, b:i32, c:i32) -> i32 {
+    a+b+c
+}
+
+fn return_integer_2(a: i32, b:i32, c:i32) -> i32 {
+    a+b-c
+}
+
